70260d9bfc1b3dad1e24b77a4b6f46e4dff59f7c
mxms-IP <mxms203@gmail.com>
2026-01-01 12:05:14 +0530

    Deployment errors fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 // vite.config.js
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       'uniqid': 'uniqid'
